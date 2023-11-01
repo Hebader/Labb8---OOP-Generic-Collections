b5538c2e1490cfd2ece8e9cf085cd31bf263caa8
Heba Derawi <heba.derawi95@hotmail.com>
2023-11-01 12:32:37 +0100

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb8___OOP_Generic_Collections
+{
+    internal class Employee
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }    
+        public string Gender { get; set; }  
+        public string Salary { get; set; } 
+    }
+}
